2c0defbcbc3dda2e5c50a1100111b6630de19e6b
Jonas <wjlester77@gmail.com>
2023-07-28 19:39:36 +0800

    文章詳情render

--- controller/article.py
+++ controller/article.py
@@ -12,6 +12,7 @@ def article_detail():
     aid = request.args.get('aid')
     article = Article()
     article_content = article.get_article_detail(aid)
+    tags_list = article_content.article_tag.split(',')
     user = User()
     user_info = user.find_by_uid(article_content.uid)
     # todo 查看評論的信息
@@ -22,4 +23,6 @@ def article_detail():
         'article-detail.html',
         article=article_content,
         user=user_info,
-        collected=is_collected)
+        collected=is_collected,
+        tags_list=tags_list
+    )
