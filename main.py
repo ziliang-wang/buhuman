fe58e129e40213398a050f9654648a6c693b59ba
Jonas <wjlester77@gmail.com>
2023-07-28 20:47:12 +0800

    404錯誤處理

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
+from flask import render_template
+
 from app.app import create_app
 import logging
 
 app = create_app()
+
+
+@app.errorhandler(404)
+def not_found(err):
+    return render_template('error.html')
+
+
+@app.errorhandler(500)
+def server_err(err):
+    return render_template('error.html')
+
 
 if __name__ == '__main__':
     logging.info('info的log')
     logging.debug('debug的log')
 
     app.run(debug=True)
+
