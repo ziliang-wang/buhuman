d448519fb8077d02dc389b50c40468a63c999466
Jonas <wjlester77@gmail.com>
2023-08-07 14:48:36 +0800

    發佈文章的Modal

---
+++ template/js/new_article.js
@@ -0,0 +1,37 @@
+window.onload = () => {
+    const ue = UE.getEditor('editor', {
+        // ... 更多配置
+        shortcutMenu: false,
+        elementPathEnabled: false,
+        wordCount: false,
+        autoHeightEnabled: false,
+        // 初始化编辑器宽度,默认 1000
+        // initialFrameWidth: 800,
+        initialFrameWidth: '100%',
+        // 初始化编辑器高度,默认 320
+        initialFrameHeight: 500,
+        toolbars: [
+            [
+                // "source",
+                "insertcode",
+                "bold",
+                "italic",
+                "insertimage",
+                "link",
+                "insertorderedlist",
+                "insertunorderedlist",
+                "blockquote",
+                "undo",
+                "redo",
+                "emotion"
+            ]
+        ]
+    });
+
+    $publishBtn = document.getElementById('publishBtn');
+    $publishArticleModal = document.getElementById('publishArticleModal');
+
+    $publishBtn.onclick = () => {
+        $publishArticleModal.style.display = 'block';
+    };
+};
