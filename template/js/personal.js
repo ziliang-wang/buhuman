f64702cb239fb9fae53311f2371924d7aaad6fe5
Jonas <wjlester77@gmail.com>
2023-08-19 17:19:18 +0800

    camera style 2

--- template/js/personal.js
+++ template/js/personal.js
@@ -1,5 +1,8 @@
 window.onload = () => {
     const $menu = document.getElementById('menu');
+    const $avatarBtn = document.getElementById('avatarBtn');
+    const $avatarModal = document.getElementById('avatarModal');
+    const $personalModalClose = document.querySelector('.personal-modal-close');
 
     $menu.onclick = (e) => {
         const self = e.target;
@@ -8,5 +11,13 @@ window.onload = () => {
         if (tabName) {
             location.href = `?type=${tabName}`;
         }
+    };
+
+    $avatarBtn.onclick = () => {
+        $avatarModal.style.display = 'block';
+    };
+
+    $personalModalClose.onclick = () => {
+        $avatarModal.style.display = 'none';
     };
 };
